5483f13110708168d137fdc736247f35c0926ea9
Drexleer <drexleer01@gmail.com>
2023-10-24 12:32:09 -0500

    Fixed Controller Ad

--- server/src/controllers/Ads/createAd.js
+++ server/src/controllers/Ads/createAd.js
@@ -1,18 +1,14 @@
-const NewAd = require("../../models/NewAd");
+const NewAd = require('../../models/NewAd');
 
 // Controlador para crear un anuncio
 const createAd = async (req, res) => {
   try {
     const createAd = req.body;
-    const newAdData = await NewAd.create(createAd)
-    .populate("creator") // Esto poblará los datos del profesional
-      .exec(); //ejecuta la consulta y obtiene resultados
+    const newAdData = await NewAd.create(createAd);
     res.status(201).json(newAdData);
   } catch (error) {
-    res.status(500).json({ error: "Error creating ad.", error });
+    res.status(500).json({ error: 'Error creating ad.', error });
   }
 };
 
 module.exports = createAd;
-
-
