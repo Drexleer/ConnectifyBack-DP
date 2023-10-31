f132069eff328968d06612df578effafbbc7a64a
Drexleer <drexleer01@gmail.com>
2023-10-31 12:41:34 -0500

    Delete

--- src/router/chatRoute.js
+++
@@ -1,8 +0,0 @@
-const chatRoute = require('express').Router();
-const getMessages = require('../controllers/Chat/getAllMessages');
-const saveMessage = require('../controllers/Chat/saveMessage');
-
-chatRoute.get('/messages', getMessages);
-chatRoute.post('/save', saveMessage);
-
-module.exports = chatRoute;
