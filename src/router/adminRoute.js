6c8ac3d004b7136e34bd5b513fdae1f1cba1026c
Drexleer <drexleer01@gmail.com>
2023-11-10 15:35:36 -0500

    delete

--- src/router/adminRoute.js
+++
@@ -1,8 +0,0 @@
-const adminRoute = require('express').Router();
-const adminRegister = require('../controllers/Admin/adminRegister');
-
-adminRoute.post('/comments');
-
-adminRoute.post('/register', adminRegister);
-
-module.exports = adminRoute;
