e7b195e85992adf443d19c6e2a967da6226c7ee8
Drexleer <drexleer01@gmail.com>
2023-11-15 12:58:38 -0500

    favoritos martin

--- src/controllers/Favorites/removeFavorites.js
+++ src/controllers/Favorites/removeFavorites.js
@@ -1,4 +1,4 @@
-const Favorites = require("../../models/Favorites");
+const Favorites = require('../../models/Favorites');
 
 const removeFavorite = async (req, res) => {
   try {
@@ -11,17 +11,19 @@ const removeFavorite = async (req, res) => {
 
     if (!existingFavorite) {
       return res.status(404).json({
-        message: "El profesional no se encuentra en la lista de favoritos",
+        message: 'El profesional no se encuentra en la lista de favoritos',
       });
     }
 
     const result = await Favorites.deleteOne({ _id: existingFavorite._id });
 
-    const newFavorite = await Favorites.find().populate("professional").exec();
+    const newFavorite = await Favorites.find({ client: clientId })
+      .populate('professional')
+      .exec();
 
     res.status(200).json(newFavorite);
   } catch (error) {
-    res.status(500).json({ error: "Error del servidor" });
+    res.status(500).json({ error: 'Error del servidor' });
   }
 };
 
