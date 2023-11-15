887c57dae60e19a640cdc40a233efc3253ddf35e
Drexleer <drexleer01@gmail.com>
2023-11-15 12:44:43 -0500

    Cambios German

--- src/router/paymentsRoute.js
+++ src/router/paymentsRoute.js
@@ -1,14 +1,14 @@
-const paymentsRoute = require("express").Router();
-const paymentsRegister = require("../controllers/Payment/paymentsRegister");
-const paymentsByUser = require("../controllers/Payment/paymentsByUser");
-const paymentsGet = require("../controllers/Payment/paymentGet");
-const paymentCheck = require("../controllers/Payment/paymentCheck");
+const paymentsRoute = require('express').Router();
+const paymentsRegister = require('../controllers/Payment/paymentsRegister');
+const paymentsByUser = require('../controllers/Payment/paymentsByUser');
+const paymentsGet = require('../controllers/Payment/paymentGet');
+const paymentCheck = require('../controllers/Payment/paymentCheck');
+const paymentsToProfs = require('../controllers/Payment/paymentsToProfs');
 
-paymentsRoute.post("/register", paymentsRegister); 
-paymentsRoute.get("/search/:userName", paymentsByUser); 
-paymentsRoute.get("/", paymentsGet); 
-paymentsRoute.get("/check/:paymentID", paymentCheck); 
-
+paymentsRoute.post('/register', paymentsRegister);
+paymentsRoute.get('/search/:userName', paymentsByUser);
+paymentsRoute.get('/search/professionals/:_id', paymentsToProfs);
+paymentsRoute.get('/', paymentsGet);
+paymentsRoute.get('/check/:paymentID', paymentCheck);
 
 module.exports = paymentsRoute;
-
