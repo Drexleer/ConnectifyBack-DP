c08ef2d9ce8136aaee51193c02c0e9ec6d298f32
Drexleer <drexleer01@gmail.com>
2023-11-15 20:38:35 -0500

    Borrado profesional

--- src/controllers/Ads/getAllAds.js
+++ src/controllers/Ads/getAllAds.js
@@ -3,7 +3,10 @@ const NewAd = require('../../models/NewAd');
 const getAllAds = async (req, res) => {
   try {
     const ads = await NewAd.find({ isDeleted: false })
-      .populate('creator')
+      .populate({
+        path: 'creator',
+        match: { deleted: { $ne: true } }, // Excluir los creadores con deleted: true
+      })
       .exec();
     res.status(200).json(ads);
   } catch (error) {
