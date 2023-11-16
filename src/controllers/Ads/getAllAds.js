32cf7f0573fc5932be6128647e144bdbe2aa17ba
Drexleer <drexleer01@gmail.com>
2023-11-15 20:47:14 -0500

    Borrado

--- src/controllers/Ads/getAllAds.js
+++ src/controllers/Ads/getAllAds.js
@@ -3,11 +3,12 @@ const NewAd = require('../../models/NewAd');
 const getAllAds = async (req, res) => {
   try {
     const ads = await NewAd.find({ isDeleted: false })
-      .populate({
-        path: 'creator',
-        match: { deleted: { $ne: true } }, // Excluir los creadores con deleted: true
-      })
+      .populate('creator')
       .exec();
+
+    const adsFiltrados = ads.filter((ad) => ad.creator.isDeleted !== true);
+    res.status(200).json(adsFiltrados);
+
     res.status(200).json(ads);
   } catch (error) {
     res.status(500).json({ error: 'Error creating ad.' });
