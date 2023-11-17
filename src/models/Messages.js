c6c701716f61593d0c62c0c3a79cb01b3d4242a5
Drexleer <drexleer01@gmail.com>
2023-11-17 10:55:50 -0500

    Modelo

--- src/models/Messages.js
+++ src/models/Messages.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const MessagesMailSchema =  new Schema ({
-    name:{
-        type: String,
-    },
-    email:{
-        type:String,
-    },
-    message:{
-        type: String,
-    }
-})
+const newMessagesSchema = new Schema({
+  message: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  timestamp: {
+    type: Number,
+    default: Math.floor(Date.now() / 1000),
+  },
+});
 
-module.exports = mongoose.model('MessagesMail' , MessagesMailSchema)
+module.exports = mongoose.model('NewMessages', newMessagesSchema);
